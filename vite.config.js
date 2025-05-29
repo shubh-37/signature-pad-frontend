2c6c05cc4d10a6da01b6c9a6b8bd6327c106f81e
Shubh Arya <shubh.arya37@gmail.com>
2025-05-29 14:54:11 +0530

    tailwind installation

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ['pdfjs-dist/build/pdf.worker.min.js'],
   },
