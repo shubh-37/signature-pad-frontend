ecfdfd872ef841ec325575bdb94d5feea68041e2
Shubh Arya <shubh.arya37@gmail.com>
2025-05-30 16:48:46 +0530

    added shadcn components

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path';
 
 export default defineConfig({
   base: './',
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ['pdfjs-dist/build/pdf.worker.min.js'],
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
